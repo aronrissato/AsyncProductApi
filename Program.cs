a1c66d19d271c81bb5a9b911fbc32c09edd8fc27
Aron Rissato <aronhrissato@gmail.com>
2025-09-24 00:43:07 -0300

    Creating a status endpoint

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
 using AsyncProductApi.Data;
+using AsyncProductApi.Dtos;
 using AsyncProductApi.Models;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<AppDbContext>(
-    opt => 
-        opt.UseSqlite("Data Source=RequestDB.db;"));
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlite("Data Source=RequestDB.db;"));
 
 var app = builder.Build();
 
@@ -15,16 +15,36 @@ app.UseHttpsRedirection();
 // Start endpoint
 app.MapPost("api/v1/products", async (AppDbContext dbContext, ListingRequest request) =>
 {
-    if (request == null)
-        return Results.BadRequest();
-    
     request.RequestStatus = "Accept";
     request.EstimatedCompletionTime = "2025-08-22:14:00:00";
-    
+
     await dbContext.ListingRequests.AddAsync(request);
     await dbContext.SaveChangesAsync();
-    
+
     return Results.Accepted($"api/v1/productstatus/{request.RequestId}", request);
+});
+
+// Status endpoint
+app.MapGet("api/v1/productstatus/{requestId}", (AppDbContext context, string requestId) =>
+{
+    var request = context.ListingRequests.FirstOrDefault(x => x.RequestId == requestId);
+    if (request == null)
+        return Results.NotFound();
+
+    ListingStatus listingStatus = new()
+    {
+        RequestStatus = request.RequestStatus,
+        ResourceUrl = string.Empty
+    };
+
+    if (request.RequestStatus!.ToUpper() == "COMPLETE")
+    {
+        listingStatus.ResourceUrl = $"api/v1/products/{Guid.NewGuid().ToString()}";
+        return Results.Ok(request);
+    }
+
+    listingStatus.EstimatedCompletionTime = "2025-08-22:14:00:00";
+    return Results.Ok(request);
 });
 
 app.Run();
